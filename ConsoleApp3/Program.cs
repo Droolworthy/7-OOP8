ad8cd3cc09b2d1b07f75f76094b671df95a31df7
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2023-04-11 19:15:45 +0300

    Update Program.cs

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,5 +1,3 @@
-﻿using System;
-
 namespace OOP8
 {
     internal class Program
@@ -42,11 +40,11 @@ namespace OOP8
         {
             List<Fighter> fighters = new List<Fighter>();
 
-            fighters.Add(new Wrestler("Хабиб", 1000, 20));
-            fighters.Add(new Kickboxer("Жан-Клод Ван Дамм", 1000, 15));
-            fighters.Add(new Boxer("Тайсон", 100, 20));
-            fighters.Add(new Karateka("Ип Ман", 100, 17));
-            fighters.Add(new TaekwondoPractitioner("Марк Дакаскос", 100, 16));
+            fighters.Add(new Wrestler("Хабиб", 100, 15, 100));
+            fighters.Add(new Kickboxer("Жан-Клод Ван Дамм", 100, 15));
+            fighters.Add(new Boxer("Тайсон", 100, 20, 67));
+            fighters.Add(new Karateka("Ип Ман", 100, 10));
+            fighters.Add(new TaekwondoPractitioner("Марк Дакаскос", 100, 20));
 
             Console.WriteLine();
 
@@ -67,7 +65,7 @@ namespace OOP8
 
                         fighters.Remove(rightFighter);
 
-                        Console.WriteLine($"\nВы выбрали первого бойца - {rightFighter.Name}.");
+                        Console.WriteLine($"\nВы выбрали первого бойца - {rightFighter.NameWarrior}.");
                         Console.Clear();
 
                         ShowListFighters(fighters);
@@ -79,7 +77,7 @@ namespace OOP8
 
                         if (isSuccess)
                         {
-                            for(int j = 0; j < fighters.Count; j++)
+                            for (int j = 0; j < fighters.Count; j++)
                             {
                                 if (leftFighterIndex == j)
                                 {
@@ -87,25 +85,37 @@ namespace OOP8
 
                                     fighters.Remove(leftFighter);
 
-                                    Console.WriteLine($"\nВы выбрали второго бойца - {rightFighter.Name}.");
+                                    Console.WriteLine($"\nВы выбрали второго бойца - {rightFighter.NameWarrior}.");
                                     Console.WriteLine("\nБОЙ НАЧИНАЕТСЯ!");
                                     Console.WriteLine();
 
-                                    while (rightFighter.Health > 0 && leftFighter.Health > 0)
+                                    while (rightFighter.HealthWarrior >= 0 && leftFighter.HealthWarrior >= 0)
                                     {
-                                        rightFighter.TakeDamage(leftFighter.DealDamage());
-                                        leftFighter.TakeDamage(rightFighter.DealDamage());
+                                        rightFighter.TakeDamage(leftFighter.DealDamage(rightFighter));
+                                        leftFighter.TakeDamage(rightFighter.DealDamage(leftFighter));
                                         rightFighter.ShowInfoFighter();
                                         leftFighter.ShowInfoFighter();
 
+                                        Console.WriteLine("---------------------------------------------------");
                                         Console.ReadKey();
+
+                                        if (rightFighter.HealthWarrior <= 0)
+                                        {
+                                            Console.WriteLine("Победа бойца - " + rightFighter.NameWarrior);
+                                            return;
+                                        }
+                                        else if (leftFighter.HealthWarrior <= 0)
+                                        {
+                                            Console.WriteLine("Победа бойца - " + leftFighter.NameWarrior);
+                                            return;
+                                        }
                                     }
                                 }
                                 else
                                 {
                                     Console.WriteLine("Ошибка. Данный номер отсутствует.");
                                 }
-                            }                        
+                            }
                         }
                     }
                     else
@@ -132,85 +142,110 @@ namespace OOP8
 
     class Fighter
     {
-        protected string _name;
-        protected int _health;
-        protected int _damage;
+        protected string _nameWarrior;
+        protected int _healthWarrior;
+        protected int _damageWarrior;
 
-        public Fighter(string name, int health, int damage)
+        public Fighter(string nameCombatant, int healthCombatant, int damageCombatant)
         {
-            _name = name;
-            _health = health;
-            _damage = damage;
+            _nameWarrior = nameCombatant;
+            _healthWarrior = healthCombatant;
+            _damageWarrior = damageCombatant;
         }
 
-        public string Name
+        public string NameWarrior
         {
             get
             {
-                return _name;
+                return _nameWarrior;
             }
         }
 
-        public int Health
+        public int HealthWarrior
         {
             get
             {
-                return _health;
+                return _healthWarrior;
             }
         }
 
         public virtual void TakeDamage(int damage)
         {
-            _health -= _damage;
+            _healthWarrior -= damage;
         }
 
-        public virtual int DealDamage()
+        public virtual int DealDamage(Fighter fighter)
         {
-            return _damage;
+            return _damageWarrior;
         }
 
-        public void ShowInfoFighter()
+        public virtual void ShowInfoFighter()
         {
-            Console.WriteLine("Имя - " + _name + ", Здоровье - " + _health + " хп; " + "Урон - " + _damage);
+            Console.WriteLine("Имя - " + _nameWarrior + ", Здоровье - " + _healthWarrior + " хп; " + "Урон - " + _damageWarrior + ";");
         }
     }
 
     class Wrestler : Fighter
     {
-        private int _fighterMinimumStamina = 0;
-        private int _fighterMaximumStamina = 100;
+        private int _wrestlerEndurance = 100;
+        private int _usingSkillDoubleAttack = 25;
 
-        public Wrestler(string name, int health, int damage) : base(name, health, damage) { }
-
-        public override void TakeDamage(int damage)
+        public Wrestler(string name, int health, int damage, int endurance) : base(name, health, damage)
         {
-            base.TakeDamage(damage);
+            _wrestlerEndurance = endurance;
         }
 
-        public override int DealDamage()
+        public override void ShowInfoFighter()
         {
-            
+            base.ShowInfoFighter();
+            Console.WriteLine("Выносливость - " + _wrestlerEndurance);
         }
 
-        private int DoubleAttack()
+        public override int DealDamage(Fighter fighter)
         {
-            
+            return UseDoubleAttack(fighter);
+        }
+
+        private int UseDoubleAttack(Fighter fighter)
+        {
+            Random random = new Random();
+
+            int activationDoubleAttack = 40;
+            int minimumActivationDoubleAttack = 1;
+            int maximumActivationDoubleAttack = 100;
+
+            if (activationDoubleAttack > random.Next(minimumActivationDoubleAttack, maximumActivationDoubleAttack))
+            {
+                _wrestlerEndurance -= _usingSkillDoubleAttack;
+
+                if (TryDoubleAttack())
+                {
+                    int damage = _damageWarrior;
+                    fighter.TakeDamage(damage);
+                    return _damageWarrior;
+                }
+                else
+                {
+                    _wrestlerEndurance = 0;
+                    return _damageWarrior;
+                }
+            }
+            else
+            {
+                return _damageWarrior;
+            }
         }
 
         private bool TryDoubleAttack()
         {
-            int enduranceDoubleAttack = 25;
-            _fighterMaximumStamina -= enduranceDoubleAttack; 
-
-            if (_fighterMaximumStamina >= _fighterMinimumStamina)
+            if (_wrestlerEndurance >= 0)
             {
-                Console.WriteLine("У " + _name + " " + _fighterMaximumStamina + " и он применяет две атаки подряд.");
-
+                Console.WriteLine("Боец " + _nameWarrior + " применяет способность двойная атака, используя " + _usingSkillDoubleAttack + "% выносливости.");
                 return true;
             }
             else
             {
-                _fighterMaximumStamina = 0;
+                _wrestlerEndurance = 0;
                 return false;
             }
         }
@@ -218,71 +253,98 @@ namespace OOP8
 
     class Kickboxer : Fighter
     {
-        private int _number = 0;
-        private int _frequencyDoubleDamage = 3;
+        private int _beginningDoubleDamageCountdown = 0; 
+        private int _endDoubleDamageCountdown = 3;
 
         public Kickboxer(string name, int health, int damage) : base(name, health, damage) { }
 
-        public override void TakeDamage(int damage)
+        public override int DealDamage(Fighter fighter)
         {
-            base.TakeDamage(damage);
+            return UseDoubleDamage();
         }
 
-        public override int DealDamage()
-        {
-            return DoubleDamage();
-        }
-
-        private int DoubleDamage()
+        private int UseDoubleDamage()
         {
             int doubleDamage = 30;
             int damage = 15;
 
-            if (_frequencyDoubleDamage <= _number)
+            if (_endDoubleDamageCountdown <= _beginningDoubleDamageCountdown)
             {
-                Console.WriteLine($"Двойной урон наносит {_name}");
+                Console.WriteLine($"Двойной урон наносит {_nameWarrior}");
 
-                if (_frequencyDoubleDamage == _number)
+                if (_endDoubleDamageCountdown == _beginningDoubleDamageCountdown)
                 {
-                    _frequencyDoubleDamage += _frequencyDoubleDamage;
+                    _endDoubleDamageCountdown += _endDoubleDamageCountdown;
                 }
 
-                return _damage = doubleDamage;
+                return _damageWarrior = doubleDamage;
             }
             else
             {
-                this._number++;
-                return _damage = damage;
+                this._beginningDoubleDamageCountdown++;
+                return _damageWarrior = damage;
             }
         }
     }
 
     class Boxer : Fighter
     {
-        public Boxer(string name, int health, int damage) : base(name, health, damage) { }
+        private int _armorProfessionalBoxer = 100;
 
-        public int Armor; //защита
-
-        public override void DealDamage(int damage)
+        public Boxer(string name, int health, int damage, int armor) : base(name, health, damage)
         {
+            _armorProfessionalBoxer = armor;
+        }
 
+        public override void ShowInfoFighter()
+        {
+            base.ShowInfoFighter();
+            Console.WriteLine("Защита - " + _armorProfessionalBoxer);
         }
 
         public override void TakeDamage(int damage)
         {
-            base.TakeDamage(damage);
-            Reflection();
+            BlockDamage(damage);
         }
 
-        private int Vampirism() //вампиризм отнимает здоровье и прибавляет его себе
+        private void BlockDamage(int damage)
         {
-            //_health += _damage;
-            //return _health;
+            Random random = new Random();
+
+            int halfBlockedDamage = 2;
+            int activatingDamageLock = 35;
+            int minimumActivatingDamageLock = 1;
+            int maximumActivatingDamageLock = 100;
+
+            if (activatingDamageLock > random.Next(minimumActivatingDamageLock, maximumActivatingDamageLock))
+            {
+                int blockedDamage = damage / halfBlockedDamage;
+                Console.WriteLine(_nameWarrior + " блокирует - " + blockedDamage + " урона.");
+                _armorProfessionalBoxer -= blockedDamage;
+
+                TryTakeDefenseDamage();
+            }
+            else
+            {
+                _armorProfessionalBoxer -= damage;
+
+                TryTakeDefenseDamage();
+            }
         }
 
-        private void Reflection() // отражает урон в противника
+        private bool TryTakeDefenseDamage()
         {
+            if (_armorProfessionalBoxer >= 0)
+            {
+                return true;
+            }
+            else
+            {
+                _healthWarrior += _armorProfessionalBoxer;
+                _armorProfessionalBoxer = 0;
+            }
 
+            return false;
         }
     }
 
@@ -290,41 +352,102 @@ namespace OOP8
     {
         public Karateka(string name, int health, int damage) : base(name, health, damage) { }
 
+        public override int DealDamage(Fighter fighter)
+        {
+            return UseBleedingDamage(fighter);
+        }
+
         public override void TakeDamage(int damage)
         {
             base.TakeDamage(damage);
-            BleedingDamage();
         }
 
-        public override void DealDamage(int damage)
+        private int UseBleedingDamage(Fighter fighter)
         {
+            Random random = new Random();
 
-        }
+            int activationBleedingDamage = 45;
+            int minimumBleedingDamage = 1;
+            int maximumBleedingDamage = 100;
+            int bleedingDamage = random.Next(10, 25);
 
-        private void BleedingDamage() // шанс на кровотечение, здоровье отнимется
-        {
-
-        }
+            if (activationBleedingDamage > random.Next(minimumBleedingDamage, maximumBleedingDamage))
+            {
+                Console.WriteLine(_nameWarrior + ", выполняет режущий удар с логтя, у противника кровотечение на - " + bleedingDamage + " урона.");
+                fighter.TakeDamage(bleedingDamage);
+                return _damageWarrior;
+            }
+            else
+            {
+                return _damageWarrior;
+            }
+        }       
     }
 
     class TaekwondoPractitioner : Fighter
     {
         public TaekwondoPractitioner(string name, int health, int damage) : base(name, health, damage) { }
 
+        public override int DealDamage(Fighter fighter)
+        {
+            return UseReflectionDamage(fighter);
+        }
+
         public override void TakeDamage(int damage)
         {
-            base.TakeDamage(damage);
-            Evasion();
+            if (TryReflectionDamageFighter())
+            {
+                UseEvasionDamage();
+            }
+            else
+            {
+                base.TakeDamage(damage);
+            }
         }
 
-        public override void DealDamage(int damage)
+        private bool TryReflectionDamageFighter()
         {
+            Random random = new Random();
 
+            int activationReflectionDamage = 30;
+            int minimumValueReflectionDamage = 1;
+            int maximumValueReflectionDamage = 100;
+
+            if (activationReflectionDamage > random.Next(minimumValueReflectionDamage, maximumValueReflectionDamage))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
-        private void Evasion() // шанс уклонится и не получить урон
+        private int UseReflectionDamage(Fighter fighter) 
         {
+            Random random = new Random();
 
+            int activationReflectionDamage = 50;
+            int minimumValueReflectionDamage = 1;
+            int maximumValueReflectionDamage = 100;
+
+            if (activationReflectionDamage > random.Next(minimumValueReflectionDamage, maximumValueReflectionDamage))
+            {
+                fighter.TakeDamage(fighter.DealDamage(fighter));
+                Console.WriteLine(_nameWarrior + " отражает атаку противника на " + fighter.DealDamage(fighter) + " урона.");
+                return _damageWarrior;
+            }
+            else
+            {
+                return _damageWarrior;
+            }
+        }
+
+        private void UseEvasionDamage()
+        {            
+            int damage = 0;
+            Console.WriteLine(_nameWarrior + " уклоняется от атаки получив " + damage + " урона.");
+            _healthWarrior -= damage;
         }
     }
 }
